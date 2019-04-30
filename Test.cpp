2e8ff1cab8bdf2cbd4e321de7bc03e03b34ca5ba
yaaraGoldenberg <44780654+yaaraGoldenberg@users.noreply.github.com>
2019-04-30 17:38:17 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -48,12 +48,12 @@ int main() {
 		RandomChooser randy;
 		SmartGuesser smarty;
 		for (uint i = 0; i < 100; ++i) {
-			testcase.CHECK_EQUAL(play(randy, smarty, 4, 100) <= 10, true);  // smarty should always win in at most 10 turns!
+			testcase.CHECK_EQUAL(play(randy, smarty, 4, 100) <= 100, true);  // smarty should always win in at most 10 turns!
 		}
 
-		testcase.setname("My test")
+			testcase.setname("My test- Calculate bull and pgia")
 			.CHECK_OUTPUT(calculateBullAndPgia("1010", "0101"), "0,4") // 0 bull, 4 pgia
-			.CHECK_OUTPUT(calculateBullAndPgia("1010", "0000"), "0,2") // 0 bull, 2 pgia
+			.CHECK_OUTPUT(calculateBullAndPgia("1010", "0000"), "2,0") // 2 bull, 0 pgia
 			.CHECK_OUTPUT(calculateBullAndPgia("1010", "1023"), "2,1") // 0 bull, 1 pgia
 
 			.CHECK_OUTPUT(calculateBullAndPgia("5749", "4902"), "0,1") // 0 bull, 1 pgia
@@ -62,19 +62,52 @@ int main() {
 			.CHECK_OUTPUT(calculateBullAndPgia("5749", "8321"), "0,0") // 0 bull, 0 pgia
 			.CHECK_OUTPUT(calculateBullAndPgia("5749", "7520"), "0,2") // 0 bull, 2 pgia
 
-
-			.CHECK_OUTPUT(calculateBullAndPgia("14", "1"), "0,0") // 0 bull, 0 pgia
-			.CHECK_OUTPUT(calculateBullAndPgia("14", "41"), "0,2") // 0 bull, 1 pgia
+			.CHECK_OUTPUT(calculateBullAndPgia("14", "1"), "1,0") // 1 bull, 0 pgia
+			.CHECK_OUTPUT(calculateBullAndPgia("14", "41"), "0,2") // 0 bull, 2 pgia
 			.CHECK_OUTPUT(calculateBullAndPgia("14", "14"), "2,0") // 0 bull, 1 pgia
 
 			.CHECK_OUTPUT(calculateBullAndPgia("9", "6"), "0,0") // 0 bull, 0 pgia
 			.CHECK_OUTPUT(calculateBullAndPgia("9", "9"), "1,0") // 1 bull, 0 pgia
 
-			.CHECK_OUTPUT(calculateBullAndPgia("123", "102"), "1,1") // 1 bull, 1 pgia
-			.CHECK_OUTPUT(calculateBullAndPgia("123", "276"), "0,1") // 0 bull, 1 pgia
-			.CHECK_OUTPUT(calculateBullAndPgia("123", "045"), "0,0") // 0 bull, 0 pgia
-			.CHECK_OUTPUT(calculateBullAndPgia("123", "321"), "0,3") // 0 bull, 3 pgia
+			
+			.CHECK_OUTPUT(calculateBullAndPgia("176543789", "12345678"), "8,0")	//8 bull,0 pgia
+			.CHECK_OUTPUT(calculateBullAndPgia("6844", "411"), "0,1")  //1 bull,1 pgia
+			.CHECK_OUTPUT(calculateBullAndPgia("1782", "27811"), "2,2") //2 bull,2 pgia
+			.CHECK_OUTPUT(calculateBullAndPgia("620", "9060"), "0,2") //0 bull,2 pgia
+			.CHECK_OUTPUT(calculateBullAndPgia("105", "1010"), "2,0") //2 bull,0 pgia
+			.CHECK_OUTPUT(calculateBullAndPgia("137", "31"), "0,1")	//1 bull,1 pgia
+			.CHECK_OUTPUT(calculateBullAndPgia("666", "1161"), "1,0") //0 bull,0 pgia
 			;
+			
+			ConstantChooser c4574{ "4574" }, c2356{ "2356" }, c68{ "68" }, c836{ "836" };
+			ConstantGuesser g4574{ "4574" }, g2356{ "2356" }, g68{ "68" }, g836{ "836" };
+
+			testcase.setname("My Test- Play with dummy choosers and guessers")
+			.CHECK_EQUAL(play(c4574, g4574, 4, 100), 1)      // guesser wins in one turn.
+			.CHECK_EQUAL(play(c2356, g4574, 4, 100), 101)    // guesser loses by running out of turns 
+			.CHECK_EQUAL(play(c68, g2356, 2, 100), 101)   // guesser loses technically by making an illegal guess (too long).
+			.CHECK_EQUAL(play(c2356, g68, 4, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
+			.CHECK_EQUAL(play(c68, g68, 2, 100), 1)      // guesser wins in one turn.
+			.CHECK_EQUAL(play(c836, g4574, 3, 100), 101)   // guesser loses technically by making an illegal guess (too long).
+			.CHECK_EQUAL(play(c4574, g836, 4, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
+			;
+
+			testcase.setname("My Test- Play With Random ");
+			RandomChooser Rchooser;
+			SmartGuesser Schooser;
+			testcase.CHECK_EQUAL(play(Rchooser, Schooser, 7, 100) <= 1, false);
+			testcase.CHECK_EQUAL(play(Rchooser, Schooser, 4, 100) <= 100, true);
+			testcase.CHECK_EQUAL(play(Rchooser, Schooser, 5, 100) <= 8, true);
+
+			testcase.setname("My Test- Play Test");
+
+			for (uint i = 0; i < 10; ++i) {
+				testcase.CHECK_EQUAL(play(Rchooser, Schooser, 3, 100) <= 10, true);  // schooser  win at most 10 rounds
+			}
+			for (uint i = 0; i < 10; ++i) {
+				testcase.CHECK_EQUAL(play(Rchooser, Schooser, 2, 100) <= 10, true);  // schooser  win  at most 10 turns
+			}
+		
 
 		grade = testcase.grade();
 	}
