2bcdf28fa878c91fa00d7c633f2f85f12c0c57b4
yaaraGoldenberg <44780654+yaaraGoldenberg@users.noreply.github.com>
2019-05-07 18:27:59 +0300

    Add files via upload

--- calculate.cpp
+++ calculate.cpp
@@ -5,21 +5,19 @@ using namespace std;
 string bullpgia::calculateBullAndPgia(string s, string guess) {
 	int countBull = 0, countPgia = 0, size = s.length();
 	for (int i = 0; i < size; i++) {
+		if (s[i] == guess[i]) {
+			countBull++;
+			s[i] = 'T';
+			guess[i] = 'Z';
+		}
+	}
+	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
-			if (s.at(i) == guess.at(j)) {
-				if (i == j) {
-					countBull++;
-					break;
-				}
-				else {
-					countPgia++;
-				}
+			if (s[i] == guess[j]) {
+				countPgia++;
+				guess[j] = 'Z';
 			}
 		}
 	}
-	string ans = to_string(countBull) + "," + to_string(countPgia);
-	return ans;
+	return to_string(countBull) + "," + to_string(countPgia);
 }
-
-
-
